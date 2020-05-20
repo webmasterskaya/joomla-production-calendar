b7cb590e0b33370bb4cbe391182779bc1ebb20b3
Artem Vasilev <kern.usr@gmail.com>
2020-05-20 12:36:06 +0300

    copyright

--- ProductionCalendar.php
+++ ProductionCalendar.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * @package    Joomla.Library - Production calendar
+ * @version    __DEPLOY_VERSION__
+ * @author     Artem Vasilev - webmasterskaya.xyz
+ * @copyright  Copyright (c) 2018 - 2020 Webmasterskaya. All rights reserved.
+ * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link       https://webmasterskaya.xyz/
+ */
+
+defined('_JEXEC') or die;
 
 require_once dirname(__FILE__) . '/carono/Calendar.php';
 
